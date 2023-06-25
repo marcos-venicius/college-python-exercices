bc4563a912edb87d41ff217ea657865a6d27da1d
Marcos Venicus <marcosdevelopment.business@gmail.com>
2023-06-25 11:23:11 -0300

    ask user for a year git diff --stagedgit diff --stagedfake datagit diff --stagedgit diff --staged

--- data-science/fake-dengue/regression.py
+++ data-science/fake-dengue/regression.py
@@ -4,6 +4,18 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
 import pandas as pd
 
+def get_year(years) -> int:
+    min_year = int(years.iloc[0]['year']) + 1
+
+    year = input('prediction year: ')
+
+    if year is None or not year.isnumeric() or int(year) < min_year:
+        print(f'[!] invalid year. must be greater than or equal to {min_year}')
+        return get_year(years)
+
+    return int(year)
+
+
 DATA_FILE_NAME = 'data.csv'
 
 file = pd.read_csv(DATA_FILE_NAME)
@@ -14,18 +26,20 @@ cases = file[['cases']]
 linearRegression = LinearRegression()
 linearRegression.fit(X=years, y=cases)
 
-future_year = [[2018]]
-cases_in_2018 = linearRegression.predict(future_year)
+prediction_year = get_year(years)
 
-print('cases expected for 2018:', int(cases_in_2018))
+future_year = [[prediction_year]]
+cases_in_prediction_year = linearRegression.predict(future_year)
+
+print(f'cases expected for {prediction_year}:', int(cases_in_prediction_year))
 
 plt.scatter(years, cases, color='black')
-plt.scatter(future_year, cases_in_2018, color='red')
+plt.scatter(future_year, cases_in_prediction_year, color='red')
 plt.plot(years, linearRegression.predict(years), color='blue')
 
 plt.xlabel('Years')
 plt.ylabel('Cases')
-plt.xticks([2018])
-plt.yticks([int(cases_in_2018)])
+plt.xticks([prediction_year])
+plt.yticks([int(cases_in_prediction_year)])
 
 plt.show()
